b32c4deea6c43dea97caf643fd8424491932d1ab
Amr Halawa <amrhalawa292@gmail.com>
2023-05-03 09:50:32 +0800

    packages & scope

---
+++ ch2pkg/main/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	// host/userORorganization/project/(dir)/package
+)
+
+
+var five = 5 
+
+func main() {
+
+	{
+		var integer = 2
+		fmt.Println(integer) // 2 
+	}
+
+	var integer = 1
+	
+	fmt.Println("Hello") // Hello
+	fmt.Println(integer) // 1
+	fmt.Println(five) // 5
+	fmt.Println(integers.Three) // 3
+
+
+}
+
+func nonMain() {
+	fmt.Println(five) // 5
+}
+
+// block --> package --> universe
